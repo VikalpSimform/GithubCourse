a57306895c4e40ed306e7c53d9749ef836cfebda
Vikalp Panchal <vikalp@simformsolutions.com>
2026-05-22 15:34:52 +0530

    Add validation for CreateUserDto and log additional agent session and tool usage entries

---
+++ Validators/CreateUserDtoValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using GithubCourse.Models;
+
+namespace GithubCourse.Validators;
+
+public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+{
+    public CreateUserDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MinimumLength(2)
+            .WithMessage("Name must be at least 2 characters long.")
+            .MaximumLength(100)
+            .WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Email must be a valid email address.");
+    }
+}
